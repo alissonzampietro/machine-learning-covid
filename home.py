1a049d53f1a1e3ce5c1e721b0aee807d4f818afb
Alisson Zampietro <AlissonZampietro.DECSKILL@edp.pt>
2020-04-22 00:45:06 +0100

    first

---
+++ home.py
@@ -0,0 +1,11 @@
+import numpy as np #scientific library to work with array and matrixes
+import zipfile #access and unzip files
+import pandas as pd #library to access databases
+import glob #read disk files
+import json #read files in json format
+import seaborn as sns #generate graphs
+import spacy #natural language processing
+import nltk #natural language processing
+from IPython.core.display import HTML #build html pages
+from matplotlib import pyplot as plt #generate graphs
+import scispacy # library to handle with specific content 
